691ad8780de7e241110c3eb3488aac2e37979ffd
rikhitha <45781265+rikhitha@users.noreply.github.com>
2021-07-23 23:42:17 +0530

    Add files via upload

---
+++ PostGetMapping/helloworld/UserDaoService.java
@@ -0,0 +1,47 @@
+package com.javatpoint.server.main.user;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.springframework.stereotype.Component;
+@Component
+public class UserDaoService 
+{
+public static int usersCount=5;
+
+private static List<User> users=new ArrayList<>();
+
+static
+{
+
+users.add(new User(1, "John", new Date()));
+users.add(new User(2, "Robert", new Date()));
+users.add(new User(3, "Adam", new Date()));
+users.add(new User(4, "Andrew", new Date()));
+users.add(new User(5, "Jack", new Date()));
+}
+
+public List<User> findAll()
+{
+return users;
+}
+
+public User save(User user)
+{
+if(user.getId()==null)
+{
+user.setId(++usersCount);
+}
+users.add(user);
+return user;
+}
+
+public User findOne(int id)
+{
+for(User user:users)
+{
+if(user.getId()==id)
+return user;
+}
+return null;
+}
+}
